d5e9469cdc4e0ccb818debca8ce916990ce02b9d
ab <abazovaa@sch1595.com>
2019-01-22 17:49:42 +0300

    3

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 var age = 35;
-    
+
     if (age >= 1 && age <= 17) {
       console.log ("Учеба");
     }
-    
+
     else if (age >= 18 && age <= 64  ){
        console.log ("Работа");
-          } 
-          
+          }
+
      else if (age >= 65 && age <= 100  ) {
          console.log ("Пенсия");
-     }  
-     
+     }
+
       else {
         console.log ("Неправильный диапазон");
      }
@@ -20,35 +20,35 @@ var age = 35;
 
 var num = [1,2,3,4,5,6,7,8,9]
 
-for (var i = 0; i < 11; i++){
+for (var i = 0; i < num.length; i++){
     if (num[i] = true)
 
     switch (i) {
-  case 1:
+  case 0:
     console.log( '1 разработчик' );
          break;
-  case 2:
+  case 1:
     console.log( '2 разработчикa' );
          break;
-  case 3:
+  case 2:
     console.log( '3 разработчикa' );
          break;
-  case 4:
+  case 3:
     console.log( '4 разработчикa' );
          break;
-  case 5:
+  case 4:
     console.log( '5 разработчиков' );
          break;
-  case 6:
+  case 5:
     console.log( '6 разработчиков' );
          break;
-  case 7:
+  case 6:
     console.log( '7 разработчиков' );
          break;
-  case 8:
+  case 7:
     console.log( '8 разработчиков' );
          break;
-  case 9:
+  case 8:
     console.log( '9 разработчиков' );
          break;
 }
